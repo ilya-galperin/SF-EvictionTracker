154db0b5f8968c1d0b007fe128132c63ba391c83
ilya-galperin <65326615+ilya-galperin@users.noreply.github.com>
2020-06-11 22:56:39 -0700

    Update soda_to_s3_operator.py

--- dags/operators/soda_to_s3_operator.py
+++ dags/operators/soda_to_s3_operator.py
@@ -1,12 +1,3 @@
-"""
-You can create any operator you want by extending the airflow.models.baseoperator.BaseOperator.
-
-There are two methods that you need to override in a derived class:
-
-Constructor - Define the parameters required for the operator. You only need to specify the arguments specific to your operator. Use @apply_defaults decorator function to fill unspecified arguments with default_args. You can specify the default_args in the dag file. See Default args for more details.
-
-Execute - The code to execute when the runner calls the operator. The method contains the airflow context as a parameter that can be used to read config values.
-"""
 # echo "" > /home/airflow/airflow/dags/operators/soda_to_s3_operator.py
 # nano /home/airflow/airflow/dags/operators/soda_to_s3_operator.py
 
@@ -14,8 +5,8 @@ import json
 
 from airflow.models.baseoperator import BaseOperator
 from airflow.utils.decorators import apply_defaults
-from airflow.hooks.S3_hook import S3Hook
 from airflow.hooks.http_hook import HttpHook
+from airflow.hooks.S3_hook import S3Hook
 
 
 class SodaToS3Operator(BaseOperator):
@@ -37,18 +28,27 @@ class SodaToS3Operator(BaseOperator):
 		
 		super().__init__(*args, **kwargs)
 		
-		self.method = method 
-		self.http_conn_id = http_conn_id
 		self.endpoint = endpoint
 		self.data = data
 		self.headers = headers
+		self.method = method
+		self.http_conn_id = http_conn_id
 	
 	
 	def execute(self, context):
+
 		soda = HttpHook(method=self.method, http_conn_id=self.http_conn_id)
-		response = soda.run(endpoint=self.endpoint, data=self.data, headers=self.headers)
 		
+		if self.data:
+			soql_filter = self.data
+		else:
+			soql_filter = """$query=SELECT:*,* WHERE :created_at > '2020-04-04' OR :updated_at > '2020-04-04'
+							   ORDER BY :id LIMIT 100"""
+							   
+		response = soda.run(endpoint=self.endpoint, data=soql_filter, headers=self.headers)
+			
 		print('=================== start ===================')
+		print(soql_filter)
 		print(response.headers)
-		print(response.json())
+		# print(response.json())
 		print('=================== done ===================')
