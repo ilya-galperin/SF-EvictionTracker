33562d64a08244d2fdafc19397d4e4c7a1c93f6b
ilya-galperin <65326615+ilya-galperin@users.noreply.github.com>
2020-07-02 16:26:53 -0700

    Update trunc_target_tables.sql

--- dags/sql/trunc_target_tables.sql
+++ dags/sql/trunc_target_tables.sql
@@ -1,5 +1,5 @@
--- echo "" > /home/airflow/airflow/dags/sql/trunc_target_tables.sql.sql
--- nano /home/airflow/airflow/dags/sql/trunc_target_tables.sql.sql
+-- echo "" > /home/airflow/airflow/dags/sql/trunc_target_tables.sql
+-- nano /home/airflow/airflow/dags/sql/trunc_target_tables.sql
 TRUNCATE TABLE raw.soda_evictions;
 TRUNCATE TABLE raw.district_data;
 TRUNCATE TABLE raw.neighborhood_data;
